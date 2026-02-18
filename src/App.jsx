03b5af49336d23cbdbac134a955bc3d318e9201e
TrashedPanda2 <therealtrashedpanda@gmail.com>
2026-02-17 23:25:05 -0500

    Import SpeedInsights from @vercel/speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CamoTracker from './components/CamoTracker';
 import Credits from './components/Credits';
 import DiscordSupportModal from './components/DiscordSupportModal';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './App.css';
 
 function AppContent() {
